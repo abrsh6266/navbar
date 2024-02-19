d2785d347f4dcc93d91d7ec66f665684d7dae084
Abrham <abrsh6266@gmail.com>
2024-02-19 09:47:16 -0900

    creating navbar component

---
+++ src/Navbar.jsx
@@ -0,0 +1,36 @@
+import { social, links } from "./data";
+import { FaBars } from "react-icons/fa";
+import logo from "./logo.svg";
+import { useState } from "react";
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+  return (
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} className="logo" alt="logo" />
+        <button onClick={toggleLinks} className="nav-toggle">
+          <FaBars />
+        </button>
+      </div>
+      {showLinks && (
+        <div className="links-container">
+          <ul className="links">
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
